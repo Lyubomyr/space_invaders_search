b9856116679b2b87e60bd07212c5db2000588a85
Lyubomyr <fedorniakl@gmail.com>
2022-07-07 15:13:38 +0300

    Improve output

--- app/services/io/print_radar.rb
+++ app/services/io/print_radar.rb
@@ -15,7 +15,7 @@ class IO::PrintRadar
       if invader_location
         invader_matrix = invaders_matrix[invader_location[:invader_index]].matrix
         invader_element = invader_matrix[row - invader_location[:rows_range].first, column - invader_location[:columns_range].first]
-        invader_element = found_invaders.key(invader_location) if invader_element
+        invader_element = found_invaders.key(invader_location) + 1 if invader_element
       end
 
       print_element = invader_element || '-'
